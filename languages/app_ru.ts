baa4a5da24e2caf68aad261a3793dcb0f6d505e9
AstyCo <Astyco@hotmail.com>
2016-06-03 16:56:28 +0300

    backup style

---
+++ languages/app_ru.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Окно</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="24"/>
+        <source>New student</source>
+        <translation>Новый студент</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <source>Load</source>
+        <translation>Загрузить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="77"/>
+        <source>Reading error</source>
+        <translation>Ошибка чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="77"/>
+        <source>Read corrupted data</source>
+        <translation>Чтение поврежденных данных</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>Open error</source>
+        <translation>Ошибка открытия</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>File &quot;%1&quot; cannot be opened</source>
+        <translation>Файл &quot;%1&quot; не может быть открыт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <source>File &quot;%1&quot; saved</source>
+        <translation>файл &quot;%1&quot; сохранен</translation>
+    </message>
+</context>
+<context>
+    <name>StudentModel</name>
+    <message>
+        <location filename="studentmodel.cpp" line="102"/>
+        <source>id</source>
+        <translation>Номер</translation>
+    </message>
+    <message>
+        <location filename="studentmodel.cpp" line="104"/>
+        <source>fio</source>
+        <translation>Фамилия</translation>
+    </message>
+</context>
+<context>
+    <name>loadDialog</name>
+    <message>
+        <location filename="load.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Выбор файла</translation>
+    </message>
+</context>
+</TS>
